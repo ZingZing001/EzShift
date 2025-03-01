037fe3f577639c6f46c9698d1b44f59a4aac2212
ayang <ayan607@aucklanduni.ac.nz>
2025-03-02 02:35:48 +1300

    made the progress bar dynamic based on the percentage

--- js/overview.js
+++ js/overview.js
@@ -1,11 +1,27 @@
-let numberEle = document.getElementById('number');
-let counter = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  let numberEle = document.getElementById('number');
+  let counter = 0;
+  numberEle.innerText = `${counter}%`;
 
-setInterval(() => {
-  if (counter == 50) {
-    clearInterval;
-  }else{
-    counter++;
-    numberEle.innerText = `${counter}%`;
-  }
-}, 2000/50);
+  const circle2 = document.querySelector(".circle-2");
+  
+
+  const percentage = 50;
+  let greenDashOffset = (450 - 450 * (percentage / 100) + 15 * (percentage / 100));
+  let greyDashOffset = 450 * percentage / 100 + 15 * (1 - percentage / 100);
+  let greyRotate = -90 + 360 * percentage / 100 + (2 - 4 * percentage / 100);
+
+  
+  circle2.setAttribute("transform", `rotate(${greyRotate})`);
+  document.documentElement.style.setProperty('--greenDashOffset', greenDashOffset);
+  document.documentElement.style.setProperty('--greyDashOffset', greyDashOffset);
+
+  setInterval(() => {
+    if (counter == percentage) {
+      clearInterval;
+    }else{
+      counter++;
+      numberEle.innerText = `${counter}%`;
+    }
+  }, 1000/percentage);
+});
